81caba3103c21385f5c1c2718dfbff8e99b2da52
unknown <nilso80@gmail.com>
2022-08-03 16:07:08 -0300

    'update'

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,7 +1,7 @@
 import { CharactersApiService } from './character/shared/characters-api.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { map } from 'rxjs/operators';
+import { map, max } from 'rxjs/operators';
 
 
 @Component({
@@ -13,8 +13,9 @@ import { map } from 'rxjs/operators';
 export class CharactersComponent implements OnInit {
 
   allCharacters: any = [];
-  personagem: any;
+  personagens: any = []
   page: number = 1;
+  maxpage:number = 0
   queryField = new FormControl();
   results: any = 0;
   values = '';
@@ -27,10 +28,12 @@ export class CharactersComponent implements OnInit {
   ngOnInit() {
     this.CharacterSvc.get().pipe(map((data: any) => data.data.results
     )).subscribe(dados => {
+      this.personagens = dados
       this.values = dados
       for(let i=0; i < dados.length; i++){
         this.data.push(dados[i])
       }
+      this.maxpage = this.data.length/10
       this.update(this.data)
     })
   }
@@ -41,31 +44,32 @@ export class CharactersComponent implements OnInit {
     this.values = event.target.value;
     this.page = 1
 
-    this.CharacterSvc.get().pipe(map((data: any) => data.data.results
-    )).subscribe(dados => {
+    this.personagens.forEach((element:any) => {
+      let nome = element.name
 
-      for(let i=0; i < dados.length; i++){
-        let nome = dados[i].name
-        if(nome.indexOf(this.values) >= 0){
-          this.data.push(dados[i])
-        }
+      if(nome.indexOf(this.values) >= 0){
+        this.data.push(element)
       }
+
       this.update(this.data)
     })
   }
 
   update(array: any){
     this.allCharacters = []
-    
+
     for(let i=0; i < array.length ; i++){
       if(i < this.page * 10 && i >= (this.page - 1) * 10){
         this.allCharacters.push(array[i])
       }
     }
+
     console.log(this.allCharacters)
   }
 
+
   nextpage(){
+    if((this.data.length/10) < this.page++)
     this.page++ 
     this.update(this.data)
   }
